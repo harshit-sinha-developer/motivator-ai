ab9a4b4602cc70ad0f4fcebd9f7f06c244dcbea5
Harshit Sinha <harshitsinha1102@gmail.com>
2023-07-03 02:08:56 +0000

    Updating logger

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 import promptSync from 'prompt-sync'
+
 import { getMotivationalQuote } from './quote_motivation/index.js'
+import logger from './utils/logger.js';
 
 dotenv.config();
 
 const prompt = promptSync({sigint: true});
 const input = prompt("Say something: ")
 
-console.log(await getMotivationalQuote(input))
+logger.info(await getMotivationalQuote(input))
 
 
 
